c67dd5a8e20ae801c91f3d5974e8b68b7210d66b
Alexis Wright <misslexiw@gmail.com>
2025-06-19 16:53:38 -0400

    final touches

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import "./App.css";
 import ContactList from "./components/contactList";
 import SelectedContact from "./components/selectedContact";
 
+const dummyContacts = [
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+];
+
 export default function App() {
   const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
       {selectedContactId ? (
-        <SelectedContact 
-        selectedContactId={selectedContactId}
-        setSelectedContactId={setSelectedContactId}
+        <SelectedContact
+          selectedContactId={selectedContactId}
+          setSelectedContactId={setSelectedContactId}
         />
       ) : (
-        <ContactList setSelectedContactId={setSelectedContactId}/>
+        <ContactList setSelectedContactId={setSelectedContactId} />
       )}
     </>
   );
--- src/components/contactList.jsx
+++ src/components/contactList.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import ContactRow from "./contactRow";
 
-
-export default function ContactList({setSelectedContactId}) {
-  const dummyContacts = [
+const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
   ];
+
+export default function ContactList({ setSelectedContactId }) {
 
   const [contacts, setContacts] = useState(dummyContacts);
 
@@ -25,7 +25,6 @@ export default function ContactList({setSelectedContactId}) {
     }
     fetchContacts();
   }, []);
-
 
   return (
     <table>
